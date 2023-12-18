8389e9fb3713320c2106abb3aabe473879cabda2
parsa <parsaddbb@gmail.com>
2023-12-18 10:34:34 +0330

    install chart js

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Bar } from "react-chartjs-2";
+const Test = () => {
+  return (
+    <div>
+      <p>
+        <Bar labels={["1", "2"]} />
+      </p>
+    </div>
+  );
+};
+export default Test;
