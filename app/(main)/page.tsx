d7ce3bc0e16d939920c9ac9d244f7975301c15c4
parsa <parsaddbb@gmail.com>
2023-12-22 20:11:59 +0330

    add link

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,25 +1,9 @@
 "use client";
-import { mainFunction, test } from "../../utils/helpers.js";
-import { useState, useEffect } from "react";
+import Link from "@/node_modules/next/link";
 export default function Home() {
-  const [answers, setAnswers] = useState([]);
-  useEffect(() => {
-    const a: number[] = test();
-    setAnswers(a);
-  }, []);
-
   return (
     <main className="flex  flex-col items-center justify-between p-4">
-      {answers.map((answer) => (
-        <div
-          key={answer.tello}
-          className="border bg-gray-100 w-full flex justify-center gap-3 text-xs "
-        >
-          <span>{answer.tello}</span>
-          <span>{answer.P}</span>
-          <span>{answer.T}</span>
-        </div>
-      ))}
+      <Link href="/test">صفحه تست</Link>
     </main>
   );
 }
